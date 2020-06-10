e3c69adf8e317401525ec6d1b69590b149980ea7
Muhammad-Tayyab1 <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-06-10 11:48:45 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Shop from './shop';
 
 function App() {
   return (
-    <div >
-     Hello World
+    <div className="App">
+      <Shop productName="Home Appliances"/>
+
     </div>
   );
 }
---
+++ src/shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Shop(props){
+    return (
+        <div>
+            <h1>This is a Online Shop for {props.productName}</h1>
+        </div>
+    );
+
+}
+export default Shop;
+ 
